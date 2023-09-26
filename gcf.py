d95c45ceb87bd96dc80ba591523a091bc58933eb
MatthewF710 <matthewf267@nycstudents.net>
2023-09-26 14:00:47 -0400

    Create gcf.py

---
+++ gcf.py
@@ -0,0 +1,16 @@
+num=int(input("Please enter a number:"))
+num2=int(input("Please enter another number:"))
+
+def factor1():
+    for i in range(1, num + 1):
+        if num%i == 0:
+            print(i)
+        else:
+            i+1
+def factor2():
+    for i in range(1, num2 + 1):
+        if num%i == 0:
+            print(i)
+        else:
+            i+1
+factor1()
