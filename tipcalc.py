5e58fa51e42645be35ba3a7f1662defb96181f54
MatthewF710 <matthewf267@nycstudents.net>
2023-09-26 14:00:50 -0400

    Update tipcalc.py

--- tipcalc.py
+++ tipcalc.py
@@ -1,4 +1,4 @@
-bill=int(input("Please enter bill cost:"))
+bill=int(input("Please enter bill cost (without $):"))
 print(bill)
 tip=input("Enter if the service was bad, okay, good, or great:")
 bad=bill*0
@@ -6,11 +6,11 @@ okay=bill*0.15
 good=bill*0.2
 great=bill*0.25
 print("This is your tip:")
-if tip == bad:
-    print(bad)
-if tip == okay:
-    print(okay)
-if tip == good:
-    print(good)
-if tip == great:
-    print(great)
+if tip == "bad":
+    print("$", bad)
+if tip == "okay":
+    print("$", okay)
+if tip == "good":
+    print("$", good)
+if tip == "great":
+    print("$", great)
