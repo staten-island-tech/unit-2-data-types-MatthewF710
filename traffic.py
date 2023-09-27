511cef9292d94cc3d9ce8163c6e4d7827d0563b2
MatthewF710 <matthewf267@nycstudents.net>
2023-09-27 13:50:37 -0400

    Create traffic.py

---
+++ traffic.py
@@ -0,0 +1,10 @@
+eastboundtraffic=True
+westboundtraffic=False
+
+if eastboundtraffic == True:
+    if westboundtraffic == True:
+        print("False")
+    if westboundtraffic == False:
+        print("True")
+else:
+    print("True")
