add0e1b6bf54bf2373ab78f3b2a7662033455351
MatthewF710 <matthewf267@nycstudents.net>
2023-10-04 14:00:34 -0400

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -1,9 +1,26 @@
+
+
 num=int(input("Please enter a number:"))
 num2=int(input("Please enter another number:"))
-
-def gcf():
-    for i in range(num, 0, -1):
-        if num%i==0 and num2%i==0:
+factors=[]
+factors2=[]
+def factor():
+    for i in range(1, num + 1):
+        if num%i == 0:
             print(i)
-            max(i)
-gcf()
+        else:
+            i+1
+
+def factor2():
+    for i in range(1, num2 + 1):
+        if num2%i == 0:
+            print(i)
+        else:
+            i+1
+factors.append(factor)
+factors2.append(factor2)
+print(factors)
+print(factors2)
+
+for i in range():
+    
