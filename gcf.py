ec05c512437fc1b45178fe41a8137f098414debd
MatthewF710 <144480602+MatthewF710@users.noreply.github.com>
2023-10-05 12:27:44 -0400

    Update gcf.py

--- gcf.py
+++ gcf.py
@@ -1,8 +1,9 @@
+#defining
 num=int(input("Please enter a number:"))
 num2=int(input("Please enter another number:"))
 factors=[]
 factors2=[]
-
+#factors
 print("CALCULATIONS:")
 def factor():
     for i in range(1, num + 1):
@@ -13,7 +14,6 @@ def factor():
             i+1
 factor()
 print(factors)
-
 def factor2():
     for i in range(1, num2 + 1):
         if num2%i == 0:
@@ -23,16 +23,14 @@ def factor2():
             i+1
 factor2()
 print(factors2)
-
+#common factors/gcf
 cf=[]
-
 for gf in factors:
     for gf2 in factors2:
         if gf==gf2:
             cf.append(gf)
 print("FINAL GCF:")
 print(max(cf))
-
 exit()
 for i in range(1, num+1):
     if (num%i==0) and (num2%i==0):
